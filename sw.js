cc926a180e5f9f84a9bcdedb10141e8d41d8b11c
Quirin Eric <quirineric@MacBook-Air-de-Quirin-2.local>
2026-07-15 15:07:08 +0300

    Add calorie goal tracking (+2kg/month target) with history chart

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "discipline-v25";
+const CACHE = "discipline-v26";
 const ASSETS = ["./", "./index.html", "./manifest.json",
   "./icons/icon-192.png", "./icons/icon-512.png", "./icons/avatar.png"];
 
