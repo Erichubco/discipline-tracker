d6cea5b6f5ac148735027aac08862977b8584a1e
Quirin Eric <quirineric@MacBook-Air-de-Quirin-2.local>
2026-07-16 01:58:15 +0300

    Require a personal API key on all data endpoints so a public repo/URL can't expose or corrupt data; add Settings field to enter the key

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "discipline-v34";
+const CACHE = "discipline-v35";
 const ASSETS = ["./", "./index.html", "./manifest.json",
   "./icons/icon-192.png", "./icons/icon-512.png", "./icons/avatar.png"];
 
