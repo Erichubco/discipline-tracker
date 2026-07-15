55f9da6f18b3362e6d2a79ba6bbbe7af4d87839d
Quirin Eric <quirineric@MacBook-Air-de-Quirin-2.local>
2026-07-15 20:47:32 +0300

    Auto-reload the page once when a new service worker takes control, so deploys self-apply instead of silently going stale

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "discipline-v33";
+const CACHE = "discipline-v34";
 const ASSETS = ["./", "./index.html", "./manifest.json",
   "./icons/icon-192.png", "./icons/icon-512.png", "./icons/avatar.png"];
 
