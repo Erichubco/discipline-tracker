729c3516947d38b551e58721c707011c56b6c4e7
Quirin Eric <quirineric@MacBook-Air-de-Quirin-2.local>
2026-07-15 20:34:50 +0300

    Remove wins-list (rejected by user), auto-link 'Lire 1 chapitre' checkbox to chapter counter so checking/unchecking the task directly drives reading progress

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "discipline-v32";
+const CACHE = "discipline-v33";
 const ASSETS = ["./", "./index.html", "./manifest.json",
   "./icons/icon-192.png", "./icons/icon-512.png", "./icons/avatar.png"];
 
