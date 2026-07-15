3ffe813ecec74013e7c162a9bae001463d981e38
Quirin Eric <quirineric@MacBook-Air-de-Quirin-2.local>
2026-07-15 15:18:22 +0300

    Improve detail discoverability: bigger menu button, live meal-moment preview in label

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "discipline-v26";
+const CACHE = "discipline-v27";
 const ASSETS = ["./", "./index.html", "./manifest.json",
   "./icons/icon-192.png", "./icons/icon-512.png", "./icons/avatar.png"];
 
