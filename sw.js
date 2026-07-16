27d4f0a7c99355b047d83ed42961c30ecda2e5c6
Quirin Eric <quirineric@MacBook-Air-de-Quirin-2.local>
2026-07-16 16:16:48 +0300

    Replace functional UI emoji with clean line-icons matching the tab bar style (settings gear, streak zap, business trending-up, reminder bell, privacy lock, storage database, book icons, export download); task labels keep their emoji as requested

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "discipline-v41";
+const CACHE = "discipline-v42";
 const ASSETS = ["./", "./index.html", "./manifest.json",
   "./icons/icon-192.png", "./icons/icon-512.png", "./icons/avatar.png"];
 
