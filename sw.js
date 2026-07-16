6914d16f1eaee226d6304619a1025ac96193882e
Quirin Eric <quirineric@MacBook-Air-de-Quirin-2.local>
2026-07-16 09:08:06 +0300

    Restore exercise technique cues (chair/bench, form tips) from the original detailed home-gym routine into the current 5-day split

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "discipline-v35";
+const CACHE = "discipline-v36";
 const ASSETS = ["./", "./index.html", "./manifest.json",
   "./icons/icon-192.png", "./icons/icon-512.png", "./icons/avatar.png"];
 
