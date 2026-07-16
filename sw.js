07ba6bdcad9f021d29cf3b57acc79d7736fd9139
Quirin Eric <quirineric@MacBook-Air-de-Quirin-2.local>
2026-07-16 14:45:11 +0300

    Fix meal menus to clearly say pick-ONE-option at every meal (breakfast was ambiguous, read like eat-everything), remove duplicate rice+chicken+veg between lunch and dinner, right-align task row buttons

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "discipline-v39";
+const CACHE = "discipline-v40";
 const ASSETS = ["./", "./index.html", "./manifest.json",
   "./icons/icon-192.png", "./icons/icon-512.png", "./icons/avatar.png"];
 
