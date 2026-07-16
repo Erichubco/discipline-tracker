d82391243475539ff33bbb355252536fa673a184
Quirin Eric <quirineric@MacBook-Air-de-Quirin-2.local>
2026-07-16 19:08:50 +0300

    Fix critical bug: logout was wiping all local data unconditionally, destroying unsynced real user data. Now only clears the session token

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "discipline-v46";
+const CACHE = "discipline-v47";
 const ASSETS = ["./", "./index.html", "./manifest.json",
   "./icons/icon-192.png", "./icons/icon-512.png", "./icons/avatar.png"];
 
